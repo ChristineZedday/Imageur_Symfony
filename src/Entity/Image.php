27ce75bdf8a551ce9566bb0c4c60e3d5d8653fe8
Christine Zedday <christine_bache@yahoo.fr>
2021-02-09 12:33:13 +0100

    first entity

---
+++ src/Entity/Image.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ImageRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ImageRepository::class)
+ */
+class Image
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $alt;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $legend;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $dossier;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getAlt(): ?string
+    {
+        return $this->alt;
+    }
+
+    public function setAlt(string $alt): self
+    {
+        $this->alt = $alt;
+
+        return $this;
+    }
+
+    public function getLegend(): ?string
+    {
+        return $this->legend;
+    }
+
+    public function setLegend(?string $legend): self
+    {
+        $this->legend = $legend;
+
+        return $this;
+    }
+
+    public function getDossier(): ?string
+    {
+        return $this->dossier;
+    }
+
+    public function setDossier(string $dossier): self
+    {
+        $this->dossier = $dossier;
+
+        return $this;
+    }
+}
