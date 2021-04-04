1c5cbd205c8a346f013d1ab1f095ff490f8b07bc
Christine Zedday <christine_bache@yahoo.fr>
2021-04-04 17:50:02 +0200

    aside

---
+++ src/Controller/AsideController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Aside;
+use App\Form\AsideType;
+use App\Repository\AsideRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/aside")
+ */
+class AsideController extends AbstractController
+{
+    /**
+     * @Route("/", name="aside_index", methods={"GET"})
+     */
+    public function index(AsideRepository $asideRepository): Response
+    {
+        return $this->render('aside/index.html.twig', [
+            'asides' => $asideRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="aside_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $aside = new Aside();
+        $form = $this->createForm(AsideType::class, $aside);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($aside);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('aside_index');
+        }
+
+        return $this->render('aside/new.html.twig', [
+            'aside' => $aside,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="aside_show", methods={"GET"})
+     */
+    public function show(Aside $aside): Response
+    {
+        return $this->render('aside/show.html.twig', [
+            'aside' => $aside,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="aside_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Aside $aside): Response
+    {
+        $form = $this->createForm(AsideType::class, $aside);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('aside_index');
+        }
+
+        return $this->render('aside/edit.html.twig', [
+            'aside' => $aside,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="aside_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Aside $aside): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$aside->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($aside);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('aside_index');
+    }
+}
---
+++ src/Entity/Aside.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AsideRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=AsideRepository::class)
+ */
+class Aside
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $titre;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $contenu;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(?string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getContenu(): ?string
+    {
+        return $this->contenu;
+    }
+
+    public function setContenu(?string $contenu): self
+    {
+        $this->contenu = $contenu;
+
+        return $this;
+    }
+}
