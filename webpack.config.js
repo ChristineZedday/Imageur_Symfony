82d95e618ff82a63760319f4cce50f4f962c68e3
Christine Zedday <christine_bache@yahoo.fr>
2021-04-05 14:13:26 +0200

    build css

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,6 @@ Encore
     // will require an extra script tag for runtime.js
     // but, you probably want this, unless you're building a single-page app
     .enableSingleRuntimeChunk()
-    .addStyleEntry('style', './assets/styles/style.css')
 
     /*
      * FEATURE CONFIG
