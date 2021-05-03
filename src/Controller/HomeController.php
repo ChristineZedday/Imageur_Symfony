2423191978137328641730ce7a9a7674f02a04fd
Christine Zedday <christine_bache@yahoo.fr>
2021-05-03 12:47:18 +0200

    nettoyage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,9 +11,6 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Entity\Nav;
-use App\Entity\Metas;
-use App\Entity\Footer;
 use App\Repository\RubriqueRepository;
 use App\Service\Generator;
 
@@ -29,45 +26,7 @@ class HomeController extends AbstractController
             'controller_name' => 'HomeController',
         ]);
     }
-      /**
-     * @Route("nav/genere/", name="nav", methods={"GET"})
-     */
-    public function navGenere(RubriqueRepository $rubriqueRepository)
-    {
-        $dir = $this->getParameter('generated_includes');
-        $nav= new Nav();
-        $nav->genereNav($dir, $rubriqueRepository);
-      
-        return $this->redirectToRoute('home');
-    }
-
-     /**
-     * @Route("footer/genere/", name="footer", methods={"GET"})
-     */
-    public function footerGenere()
-    {
-        $dir = $this->getParameter('generated_includes');
-        $text1 = $this->getParameter('footerText1');
-        $text2 = $this->getParameter('footerText2');
-        $contact =  $this->getParameter('contact');
-        $text= '<p>'.$text1.'<br/>'.$text2.'</p><p>contact: '.$contact.'</p>';
-        $footer= new Footer();
-        $footer->genereFooter($dir, $text);
-
-        return $this->redirectToRoute('home');
-    }
-      /**
-     * @Route("metas/genere/", name="metas", methods={"GET"})
-     */
-    public function metasGenere()
-    {
-        $dir = $this->getParameter('generated_includes');
-        $metas= new Metas();
-        $metas->genereMetas($dir);
-
-        return $this->redirectToRoute('home');
-    }
-
+     
    
 
     
--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -6,9 +6,6 @@ use App\Repository\ArticleRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
-use App\Entity\Nav;
-use App\Entity\Metas;
-use App\Entity\Footer;
 use App\Entity\Section;
 
 
