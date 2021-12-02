f05e639826fa99d9084d36b0d3a3f610eaac8564
Christine Zedday <57533211+ChristineZedday@users.noreply.github.com>
2021-12-02 18:38:32 +0100

    migrations nett

--- migrations/Version20210421150902.php
+++ migrations/Version20210421150902.php
@@ -26,7 +26,7 @@ final class Version20210421150902 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE article DROP javascript');
+        
         $this->addSql('ALTER TABLE article_javascript ADD CONSTRAINT FK_B83341767294869C FOREIGN KEY (article_id) REFERENCES article (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE article_javascript ADD CONSTRAINT FK_B8334176E754219D FOREIGN KEY (javascript_id) REFERENCES javascript (id) ON DELETE CASCADE');
     }
@@ -34,7 +34,7 @@ final class Version20210421150902 extends AbstractMigration
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE article ADD javascript VARCHAR(255) CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_unicode_ci`');
+      
         $this->addSql('ALTER TABLE article_javascript DROP FOREIGN KEY FK_B83341767294869C');
         $this->addSql('ALTER TABLE article_javascript DROP FOREIGN KEY FK_B8334176E754219D');
     }
