10d68b8bfba3b4987c77d36501014f081f2cc35b
Christine Zedday <57533211+ChristineZedday@users.noreply.github.com>
2021-12-03 18:33:36 +0100

    fdf

--- migrations/Version20210419501234.php
+++ migrations/Version20210419501234.php
@@ -26,7 +26,7 @@ final class Version202104119501234 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE article_javascript (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE article_javascript (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(255) NOT NULL, INDEX IDX_352C07EF7252EEEE (article_id), INDEX IDX_352C07EF7252FFFF (javascript_id),PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }  
 
     public function down(Schema $schema): void
