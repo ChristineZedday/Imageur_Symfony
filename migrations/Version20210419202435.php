8c426f75d2b00a32d7e2f60696b08887f4ed47f3
Christine Zedday <57533211+ChristineZedday@users.noreply.github.com>
2021-12-03 18:16:11 +0100

    migr

--- migrations/Version20210419202435.php
+++ migrations/Version20210419202435.php
@@ -26,7 +26,7 @@ final class Version20210419202435 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE home_page DROP contenu');
+        
         $this->addSql('ALTER TABLE section ADD home_page_id INT DEFAULT NULL, CHANGE article_id article_id INT DEFAULT NULL');
         $this->addSql('ALTER TABLE section ADD CONSTRAINT FK_2D737AEFB966A8BC FOREIGN KEY (home_page_id) REFERENCES home_page (id)');
         $this->addSql('CREATE INDEX IDX_2D737AEFB966A8BC ON section (home_page_id)');
@@ -35,7 +35,7 @@ final class Version20210419202435 extends AbstractMigration
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE home_page ADD contenu LONGTEXT CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_unicode_ci`');
+       
         $this->addSql('ALTER TABLE section DROP FOREIGN KEY FK_2D737AEFB966A8BC');
         $this->addSql('DROP INDEX IDX_2D737AEFB966A8BC ON section');
         $this->addSql('ALTER TABLE section DROP home_page_id, CHANGE article_id article_id INT NOT NULL');
